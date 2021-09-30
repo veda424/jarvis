f4358d875f19874ce74befd35f26546fad2a73d8
Vedant Agrawal <70770638+veda424@users.noreply.github.com>
2021-09-30 11:22:09 +0530

    Update google_assistant.py

--- google_assistant.py
+++ google_assistant.py
@@ -3,9 +3,11 @@ import datetime
 import speech_recognition as sr
 import webbrowser
 import os
+import cv2
+import sys
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
-engine.setProperty('voice' , voices[0].id)
+engine.setProperty('voice' , voices[1].id)
 
 
 def speak(audio):
@@ -14,7 +16,6 @@ def speak(audio):
 def takeVoice():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        speak("LISTENING....")
         print("LISTENING....")
         r.pause_threshold = 1
         r.energy_threshold = 400
@@ -22,11 +23,10 @@ def takeVoice():
     try:
         print("recognizing Your voice....")
         query = r.recognize_google(audio, language='en-in')
-        print(f"You Said:{query}")
     except Exception as e:
         print("cant recognize ur voice Please try  again")
         return "None"
-    return query
+    return query.lower()
 def playmusic(path):
     import random
     randno = random.randint(1, 10)
@@ -44,19 +44,41 @@ def greetme():
 
 if __name__ == '__main__':
     greetme()
-    greet = "Hello!!! im Shrishti how can i help you..."
+    greet = "Hello!!! This is siri how can i help you..."
     speak(greet)
     print(greet)
-    userSaid = takeVoice().lower()
-    if "open google" or "google" in userSaid:
-        webbrowser.open("www.google.com")
-    elif "open youtube" or "youtube" in userSaid:
-        webbrowser.open("www.youtube.com")
-    elif "open instagram" or "instagram" in userSaid:
-        webbrowser.open("www.instagram.com")
-    elif "open facebook" or "facebook" in userSaid:
-        webbrowser.open("www.facebook.com")
-    elif "play music" or "play songs" or "play song" in userSaid:
-        playmusic("D:\songs")
+    while True:
+        userSaid = takeVoice()
+        print(f"{userSaid}")
+        if "open google" in userSaid:
+            webbrowser.open("www.google.com")
+        elif "open youtube" in userSaid:
+            webbrowser.open("www.youtube.com")
+        elif "open instagram" in userSaid:
+            webbrowser.open("www.instagram.com")
+        elif "open facebook" in userSaid:
+            webbrowser.open("www.facebook.com")
+        elif "play music" in userSaid:
+            playmusic("D:\songs")
+        elif  "open notepad" in userSaid:
+            notepath ="C:\\Windows\\system32\\notepad.exe"
+            os.startfile(notepath)
+        elif "open cmd" in userSaid:
+            os.system("start cmd")
+        elif "open camera" in userSaid:
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow("camera",img)
+                k = cv2.waitKey(50)
+                if k == 27:
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+        elif "exit" in userSaid:
+            speak("THANK YOU")
+            sys.exit() 
 
+        else:
+            speak("sorry please say it again..")
 
